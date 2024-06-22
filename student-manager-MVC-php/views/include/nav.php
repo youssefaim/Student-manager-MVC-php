ea62e058bdc0005ab49efc9c260cd0fc9c0fe855
Youssef Aim <147152506+youssefaim@users.noreply.github.com>
2024-06-22 21:48:19 +0100

    Delete student-manager-MVC-php directory

--- student-manager-MVC-php/views/include/nav.php
+++
@@ -1,6 +0,0 @@
-<nav style="display:flex;text-decoration:none;flex-direction:row;justify-content: center;background-color:black;padding-right:2%;width:100%">
-        <ul style="list-style-type:none;display:flex;align-content: center;width:100%;color:white;gap:15%">
-            <li><a href="index.php?action=list" style="text-decoration:none;color:white;">Home</a></li>
-            <li><a href="index.php?action=create" style="text-decoration:none;color:white;">Create a student</a></li>
-        </ul>
-</nav>
